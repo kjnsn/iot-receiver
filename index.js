59fc2cc21005da93f4260caf979718a1b067b275
Kaley Main <kaleypoole17@gmail.com>
2018-11-17 11:05:20 +1100

    Add error handling/logging

--- index.js
+++ index.js
@@ -1,59 +1,67 @@
-const express = require('express');
-const net = require('net');
+const express = require("express");
+const net = require("net");
 const app = express();
-const log = require('loglevel');
-const http = require('http').Server(app);
-const io = require('socket.io')(http);
+const log = require("loglevel");
+const http = require("http").Server(app);
+const io = require("socket.io")(http);
 
-log.setLevel(process.env.LOG || 'info');
+log.setLevel(process.env.LOG || "info");
 
-const { parseInput, processInput } = require('./input');
+const { parseInput, processInput } = require("./input");
 
 let DataState = undefined;
 
 function sendState() {
-    io.emit('new state', DataState);
+  io.emit("new state", DataState);
 }
 
-app.get('/', function(req, res){
-    res.sendFile(__dirname + '/static/index.html');
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/static/index.html");
 });
 
-io.on('connection', (socket) => {
-    sendState();
-    log.debug("got socket")
-})
+io.on("connection", socket => {
+  sendState();
+  log.debug("got socket");
+});
 
 http.listen(3000, () => {
-    log.info("listening on *:3000");
+  log.info("listening on *:3000");
 });
 
 // Create the TCP server.
-const srv = net.createServer((c) => {
-    log.debug("connection recieved from", c.remoteAddress + ":" + c.remotePort);
-    let buf = "";
-    c.on('data', (data) => {
-        log.debug("received", data.length, "bytes from", c.remoteAddress + ":" + c.remotePort);
-        buf += data.toString('utf-8');
-        if (buf.includes("\n")) {
-            const msgs = buf.split("\n");
-            // Only process all but the last, as the last may not be completely received yet.
-            msgs.slice(0, msgs.length - 1).forEach(msg => {
-                // Ignore empty messages.
-                if (msg.length === 0) {
-                    return;
-                }
-                // Cut the current message out of the buffer.
-                buf = buf.slice(msg.length);
-                const parsed = parseInput(msg);
-                DataState = processInput(DataState, parsed);
-            });
-            sendState();
+const srv = net.createServer(c => {
+  log.debug("connection recieved from", c.remoteAddress + ":" + c.remotePort);
+  let buf = "";
+  c.on("data", data => {
+    log.debug(
+      "received",
+      data.length,
+      "bytes from",
+      c.remoteAddress + ":" + c.remotePort
+    );
+    buf += data.toString("utf-8");
+    if (buf.includes("\n")) {
+      const msgs = buf.split("\n");
+      // Only process all but the last, as the last may not be completely received yet.
+      msgs.slice(0, msgs.length - 1).forEach(msg => {
+        // Ignore empty messages.
+        if (msg.length === 0) {
+          return;
         }
-        
-    });
+        // Cut the current message out of the buffer.
+        buf = buf.slice(msg.length);
+        try {
+          const parsed = parseInput(msg);
+          DataState = processInput(DataState, parsed);
+        } catch (err) {
+          log.trace(err);
+        }
+      });
+      sendState();
+    }
+  });
 });
 
 srv.listen(3001, () => {
-    log.info("TCP listening on *:3001");
+  log.info("TCP listening on *:3001");
 });
